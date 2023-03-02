7a489d60ba31aec5d5c9c72280612cb135fbc5ba
gumphso <gabelot12@gmail.com>
2023-03-01 23:30:07 -0500

    Changed script to detect if library path is exported

--- launchlibexp.sh
+++ launchlibexp.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/bash 
+val="$(export -p | grep 'LD_LIBRARY_PATH')"
+if [ -z "$val" ];then
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
-echo "Library Exported for this Shell"
-echo "Type 'anaconda-navigator' to open Anaconda GUI and 'jupyter notebooks' to open notebooks"
+echo "LD_LIBRARY_PATH Exported for this Shell"
+
+else
+echo "LD_LIBRARY_PATH is already exported for this shell!"
+fi
+echo "Ensure your in the tf env by typing 'conda activate tf'"
+echo "Type 'anaconda-navigator' to open Anaconda GUI and 'jupyter notebook' to open notebooks"
 echo "DO NOT close this terminal while either is running, close through GUI"
 
